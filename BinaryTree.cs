e2792b4c84e225076fcf80379a201101da5c6485
nish159 <77575013+nish159@users.noreply.github.com>
2022-06-10 12:11:55 -0400

    Update BinaryTree.cs

--- BinaryTree.cs
+++ BinaryTree.cs
@@ -1,13 +1,22 @@
 using System;
 
-
 BinaryTree tree = new BinaryTree();
 tree.Insert(6);
 tree.PrintInOrder();
+Console.WriteLine(" ");
 tree.Insert(4);
 tree.PrintInOrder();
+Console.WriteLine(" ");
 tree.Insert(7);
 tree.PrintInOrder();
+Console.WriteLine(" ");
+tree.Insert(9);
+Console.WriteLine(" ");
+// need a variable to return a node
+var node = tree.Find(4);
+Console.WriteLine(node.value + " ");
+var max = tree.Depth();
+Console.WriteLine(max + " ");
 public class Node
 {
     public int value { get; set; }
@@ -28,6 +37,36 @@ public class BinaryTree
         PrintInOrder(root);
     }
 
+    public int Depth()
+    {
+        return Depth(root);
+    }
+
+    private int Depth(Node? node)
+    {   
+        if (node == null)
+        {
+            return 0;
+        }
+
+        int depthLeft = Depth(node.left);
+        int depthRight = Depth(node.right);
+
+        // find max of the current node and store value
+        var max = Math.Max(depthLeft, depthRight);
+        return max + 1;
+    }
+
+    /// <summary>
+    /// Finds the node in the tree with the given value
+    /// </summary>
+    /// <param name="value">The value to be found</param>
+    /// <returns>The Node with the given value if found, otherwise null</returns>
+    public Node? Find(int value)
+    {
+        return Find(root, value);
+    }
+
     public void Insert(int value)
     {
         if (root == null)
@@ -40,6 +79,34 @@ public class BinaryTree
         {
             Insert(root, value);
         }
+    }
+
+    /// <summary>
+    /// Finds the node in the tree with the given value
+    /// </summary>
+    /// <param name="node"></param>
+    /// <param name="value"></param>
+    private Node? Find(Node?  node, int value)
+    {
+        if (node == null)
+        {
+            return node;
+        }
+        
+        if (node.value == value)
+        {
+            return node;
+        }
+        else if (value > node.value)
+        {
+            // return to sync up the tree
+            return Find(node.right, value);
+        }
+        else if (value < node.value)
+        {
+            return Find(node.left, value);
+        }
+        return node;
     }
 
     /// <summary>
@@ -58,17 +125,27 @@ public class BinaryTree
         }
         else if (value < node.value /* if the value is less than the node value*/)
         {
+            if (node.left == null)
+            {
+                node.left = newNode;
+                return;
+            }
             Insert(node.left, value);
         }
         else if (value > node.value /* if the value is greater than the node value*/)
         {
+            if (node.right == null)
+            {
+                node.right = newNode;
+                return;
+            }
             Insert(node.right, value);
         }
     }
 
     private void PrintInOrder(Node? node)
     {
-        if(node == null)
+        if (node == null)
         {
             return;
         }
